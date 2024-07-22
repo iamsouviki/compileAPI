27e5cbc124d1b56d481704f5d984db5418d20e84
Souvik Ghosh <souvikghosh687@gmail.com>
2024-07-22 18:37:34 +0530

    component-config add

---
+++ src/main/java/com/wso2/choreo/sample/springboot/controller/CompilerController.java
@@ -0,0 +1,54 @@
+package com.wso2.choreo.sample.springboot.controller;
+
+
+import com.wso2.choreo.sample.springboot.model.CompileandTest;
+import com.wso2.choreo.sample.springboot.model.CompilerModel;
+import com.wso2.choreo.sample.springboot.service.CompileServices;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.UUID;
+
+@RestController
+public class CompilerController {
+
+    @Autowired
+    CompileServices compileServices;
+
+    @PostMapping(value = "/compile")
+    @CrossOrigin(origins = "*")
+    public ResponseEntity<String> compileCode(@RequestBody CompilerModel compilerModel) throws IOException {
+        try{
+            String output = compileServices.compileCode(compilerModel);
+            return new ResponseEntity<>(output, HttpStatus.OK);
+        }catch (Exception e){
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.OK);
+        }
+    }
+
+    @PostMapping(value = "/compile-and-test")
+    @CrossOrigin(origins = "*")
+    public ResponseEntity<String> compileAndTestCode(@RequestBody CompileandTest compileandTest) throws IOException {
+        CompilerModel compilerModel = new CompilerModel();
+            compilerModel.setLanguage(compileandTest.getLanguage());
+            compilerModel.setCodeText(compileandTest.getCodeText());
+            String output = compileServices.compileCode(compilerModel);
+            if(compileandTest.getExpectedOutput().equals(output)){
+                return new ResponseEntity<>("passed", HttpStatus.OK);
+            }else{
+               return new ResponseEntity<>("failed", HttpStatus.NOT_ACCEPTABLE);
+            }
+    }
+
+     @PostMapping(value = "/test")
+    @CrossOrigin(origins = "*")
+    public Integer compileAndTestCode(@RequestBody Integer compileandTest) throws IOException {
+        return compileandTest;
+    }
+}
