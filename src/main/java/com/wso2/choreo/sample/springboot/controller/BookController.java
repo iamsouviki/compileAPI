bf983a61c5fec1fbb22d58175bccf6626cc05dd9
Souvik Ghosh <souvikghosh687@gmail.com>
2024-07-22 18:45:47 +0530

    component-config add

--- src/main/java/com/wso2/choreo/sample/springboot/controller/BookController.java
+++ src/main/java/com/wso2/choreo/sample/springboot/controller/BookController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.Collection;
 
 @RestController
-@RequestMapping("/ccompile")
+@RequestMapping("/ccompiler")
 public class BookController {
 
     @Autowired
